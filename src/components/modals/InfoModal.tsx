e639e405163e53260eecbfa13bb004a6403d0f87
necropolina <git@lina.pizza>
2022-02-26 23:03:11 -0500

    info update

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -10,8 +10,9 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="How to play" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        Guess the hexadecimal color code in 6 tries. After each guess, the color of the tiles will
-        change to show how close your guess was to the color code.
+        Guess the background color's hexadecimal color code in 6 tries. 
+        After each guess, the color of the tiles will change to show how 
+        close your guess was.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
