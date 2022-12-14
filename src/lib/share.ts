bd5fe0a718df03052c26a75755b1f3c0c6b46f77
Lina <git@lina.pizza>
2022-12-14 06:34:03 +0000

    typescript package

--- src/lib/share.ts
+++ src/lib/share.ts
@@ -1,16 +1,17 @@
 import { GAME_TITLE } from '../constants/strings'
 import { solution } from './words'
 
-import nearestColor from 'nearest-color';
+import { nearestFrom } from 'nearest-colors';
 import colorNameList from 'color-name-list';
 const colors = colorNameList.reduce((o, { name, hex }) => Object.assign(o, { [name]: hex }), {});
-const nearest = nearestColor.from(colors);
+const nearest =  nearestFrom(colors);
 
 export const shareStatus = (
   guesses: string[],
   lost: boolean
 ) => {
-  const target = nearest(`#${solution}`).name;
+  const closest = nearest(`#${solution}`);
+  const target = Array.isArray(closest) ? closest[0].name : closest.name;
 
   navigator.clipboard.writeText(
     `An image depicting the outcome of a ${GAME_TITLE} game, with ${guesses.length} different-colored squares on a background the color of ${target}.\n\nGuess colors:\n${generateColorDescriptions(guesses, lost, target)}`
@@ -21,7 +22,8 @@ export const generateColorDescriptions = (guesses: string[], lost: boolean, targ
   const previous = [];
   return guesses
     .map((guess, index) => {
-      const name = nearest(`#${guess}`).name;
+      const closestColor = nearest(`#${guess}`);
+      const name = Array.isArray(closestColor) ? closestColor[0].name : closestColor.name;
       const text = `${index + 1}: ${guesses.includes(name) ? '(another) ' : ''}${name}${target === name && guess !== solution ? ' (slightly off-target)' : ''}`;
       previous.push(name);
       return text;
